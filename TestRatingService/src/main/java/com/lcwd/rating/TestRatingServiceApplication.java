8d74f2dad7b55c5dab0e2cbbdccb6be8fafef7eb
gitesh <giteshkumar99@gmail.com>
2024-12-28 20:40:03 +0530

    Added the test

--- TestRatingService/src/main/java/com/lcwd/rating/TestRatingServiceApplication.java
+++ TestRatingService/src/main/java/com/lcwd/rating/TestRatingServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestRatingServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("My first test");
 		SpringApplication.run(TestRatingServiceApplication.class, args);
 	}
 
